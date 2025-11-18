e40a738e3bbd061e8ccf398470ad638e643454d0
Nikita Gontsarov <opilane@tthk.ee>
2025-11-18 10:00:26 +0200

    php lehe struktuur on määratud

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
+<!DOCTYPE html>
+<!-- kommentaar yo -->
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Nikita Gontsarov veebirakenduste tööd</title>
+</head>
+
+<?php
+//navigeerimismenüü
+?>
 <?php
     echo "<h1>";
     echo "Tere hommikust";
     echo "</h1>";
     $tekst="TARpv24 õpilane";
     echo $tekst;
-?>
