4aebaf52c50fa622e3e4070b48ca94e23eb6d1dd
Opilane TTHK <opilane@tthk.ee>
2025-11-20 13:59:18 +0200

    ajafunktsioonid

---
+++ content/tekstifunktsioonid.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h2>Tekstifunktsioonid</h2>";
+$tekst='PHP on skriptikeel serveripoolne';
+echo $tekst;
--- nav.php
+++ nav.php
@@ -1,9 +1,29 @@
 <nav>
     <ul>
-        <li><a href="?link=avaleht.php">Avaleht</a></li>
-        <li><a href="?link=gitKasutamine.php">Git käsud</a></li>
-        <li><a href="?link=too1.php">Töö 1</a></li>
-        <li><a href="?link=muusikaKysitlus.php">Muusika kysitlus</a></li>
-        <li><a href="https://nikitagontsarov24.thkit.ee/" target="_blank">vana index</a></li>
+        <li>
+            <strong>
+                <a href="?link=avaleht.php">Avaleht</a>
+            </strong>
+        </li>
+        <li>
+            <strong>
+                <a href="?link=gitKasutamine.php">Git käsud</a>
+            </strong>
+        </li>
+        <li>
+            <strong>
+                <a href="?link=too1.php">Töö 1</a>
+            </strong>
+        </li>
+        <li>
+            <strong>
+                <a href="?link=muusikaKysitlus.php">Muusika kysitlus</a>
+            </strong>
+        </li>
+        <li>
+            <strong>
+                <a href="https://nikitagontsarov24.thkit.ee/" target="_blank">vana index</a>
+            </strong>
+        </li>
     </ul>
 </nav>
