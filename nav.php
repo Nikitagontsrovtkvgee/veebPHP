3d2259da713664f8cef7c1ef1a9f68f1acd9198d
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2026-03-01 19:54:31 +0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,29 +1,19 @@
 <nav>
     <ul>
-        <li>
-            <strong>
-                <a href="?link=avaleht.php">Avaleht</a>
-            </strong>
+        <li><a href="?link=avaleht.php">Avaleht</a></li>
+        <li><a href="?link=gitKasutamine.php">Git käsud</a></li>
+        
+        <li class="dropdown">
+            <a href="#">Funktsioonid ▼</a>
+            <ul class="dropdown-content">
+                <li><a href="?link=tekstifunktsioonid.php">Tekst</a></li>
+                <li><a href="?link=ajaFunktsioonid.php">Aeg</a></li>
+                <li><a href="?link=matem_funktsioonid.php">Matemaatika</a></li>
+            </ul>
         </li>
-        <li>
-            <strong>
-                <a href="?link=gitKasutamine.php">Git käsud</a>
-            </strong>
-        </li>
-        <li>
-            <strong>
-                <a href="?link=too1.php">Töö 1</a>
-            </strong>
-        </li>
-        <li>
-            <strong>
-                <a href="?link=muusikaKysitlus.php">Muusika kysitlus</a>
-            </strong>
-        </li>
-        <li>
-            <strong>
-                <a href="https://nikitagontsarov24.thkit.ee/" target="_blank">vana index</a>
-            </strong>
-        </li>
+
+        <li><a href="?link=galerii.php">Galerii</a></li>
+        <li><a href="?link=muusikaKysitlus.php">Muusika kysitlus</a></li>
+        <li><a href="https://nikitagontsarov24.thkit.ee/" target="_blank">vana index</a></li>
     </ul>
 </nav>
