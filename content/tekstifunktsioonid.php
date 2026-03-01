a11928dd8953100eed38120d3c3ee187c3862061
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2026-03-01 20:11:05 +0200

    Update tekstifunktsioonid.php

--- content/tekstifunktsioonid.php
+++ content/tekstifunktsioonid.php
@@ -2,30 +2,62 @@
 echo "<h2>Tekstifunktsioonid</h2>";
 
 $tekst = 'PHP on skriptikeel serveripoolne';
-echo $tekst . "<br>";
-echo "teksti pikkus - strlen() = " . strlen($tekst) . " tähemärgi<br>";
-echo "Esimesed 6 tähte - substr() = " . substr($tekst, 0, 6) . "<br>";
-echo "Sõnade arv lauses - str_word_count() = " . str_word_count($tekst) . "<br>";
-echo "ÜLA REGISTER - " . strtoupper($tekst) . "<br>";
-echo "Iga sõna algab suure tähega - " . ucwords($tekst) . "<br>";
+echo $tekst;
+echo "<br>";
+echo "teksti pikkus - strlen() = " . strlen($tekst) . " tähemärgi";
+echo "<br>";
+echo "Esimesed 6 tähte - substr() = " . substr($tekst, 0, 6);
+echo "<br>";
+echo "Alates 6 tähtest = " . substr($tekst, 6);
+echo "<br>";
+echo "Sõnade arv lauses - str_word_count() = " . str_word_count($tekst);
+echo "<br>";
+echo "ÜLA REGISTER - " . strtoupper($tekst);
+echo "<br>";
+echo "alla register - " . strtolower($tekst);
+echo "<br>";
+echo "Iga sõna algab suure tähega - " . ucwords($tekst);
+echo "<br>";
+echo "iga sõna algab väikse tähega - " . ucwords(strtolower($tekst));
+echo "<br>";
 
 $tekst2 = '         PHP on skriptikeel serveripoolne         ';
-echo "<br>|" . $tekst2 . "|<br>";
-echo "trim() – eemaldab tühikud: " . trim($tekst2) . "<br>";
+echo "<br>";
+echo "|" . $tekst2 . "|";
+echo "<br>";
+echo "trim() – eemaldab tühikud mõlemalt poolt: " . trim($tekst2);
+echo "<br>";
+echo "ltrim() – eemaldab tühikud vasakult: " . ltrim($tekst2);
+echo "<br>";
+echo "rtrim() – eemaldab tühikud paremalt: " . rtrim($tekst2);
+echo "<br>";
 
 echo "<h3>Tekst kui massiiv</h3>";
-echo "1. täht massiivist – " . $tekst[0] . "<br>";
+echo "1. täht massiivist – " . $tekst[0];
+echo "<br>";
+echo "5. täht massiivist – " . $tekst[4];
+echo "<br>";
+
+print_r(str_word_count($tekst, 1));
 $syna = str_word_count($tekst, 1);
-echo "massiivist 3. sõna – " . $syna[2] . "<br>";
+echo "<br>";
+echo "massiivist 3. sõna – " . $syna[2];
+echo "<br>";
+
+print_r(str_word_count($tekst, 2));
+echo "<br>";
 
 echo "<h2>Teksti asendamine - replace</h2>";
 $asendus = 'Javascript';
-echo "Esimesed 3 tähte asendatud: " . substr_replace($tekst, $asendus, 0, 3) . "<br>";
+
+echo "Esimesed 3 tähte asendatud: " . substr_replace($tekst, $asendus, 0, 3);
+echo "<br>";
 
 $otsi = array('PHP', 'serveripoolne');
 $asendav = array('JavaScript', 'kliendipoolne');
-echo "Teksti täielik asendus: " . str_replace($otsi, $asendav, $tekst) . "<br>";
 
+echo "Teksti täielik asendus: " . str_replace($otsi, $asendav, $tekst);
+echo "<br>";
 
 echo "<ol>";
 echo "<li>" . $tekst[0] . "</li>";
@@ -34,14 +66,13 @@ echo "<li>" . $tekst[2] . "</li>";
 echo "<li>" . $tekst[3] . "</li>";
 echo "</ol>";
 
-
 echo "<h2>MÕISTATUS – ARVA ÄRA EESTI LINNANIMI</h2>";
-$linn = "Võru"; 
-echo "<ul>";
-echo "<li>Linn algab tähega: " . substr($linn, 0, 1) . "</li>";
-echo "<li>Linna pikkus: " . strlen($linn) . " tähte</li>";
-echo "<li>Segatud tähed: " . str_shuffle($linn) . "</li>";
-echo "</ul>";
+$linn="Võru";
+echo "<ol>";
+echo "<li>Linn algab ".substr($linn, 0,1)." tähega</li>";
+echo "<li>Linnas on ".strlen($linn)." tähte</li>";
+echo "<li>Segatult tähed: ".str_shuffle($linn)."</li>";
+echo "</ol>";
 ?>
 
 <form method="post">
@@ -52,10 +83,10 @@ echo "</ul>";
 
 <?php
 if (isset($_POST["linn"])) {
-    if (ucfirst(strtolower($_POST["linn"])) == $linn) {
-        echo "<strong>" . $_POST["linn"] . " on õige!</strong>";
+    if (strtolower($_POST["linn"]) == strtolower($linn)) {
+        echo $_POST["linn"] . " on õige";
     } else {
-        echo "<strong>" . $_POST["linn"] . " on vale!</strong>";
+        echo $_POST["linn"] . " on vale!";
     }
 }
 ?>
